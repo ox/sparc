86f04b872b12b424ef00a65d287815745028405b
ArtemTitoulenko <artem.titoulenko@gmail.com>
2015-03-18 12:13:39 -0400

    Initial Commit

---
+++ Sparc/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sparc
+//
+//  Created by Artem Titoulenko on 3/18/15.
+//  Copyright (c) 2015 Artem Titoulenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
