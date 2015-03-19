c815de950acdf16feb805c4db4d5e0615c8c3915
ArtemTitoulenko <artem.titoulenko@gmail.com>
2015-03-19 14:35:19 -0400

    I think things are happening

--- Sparc/AppDelegate.swift
+++ Sparc/AppDelegate.swift
@@ -10,18 +10,41 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    
+    var statusBar = NSStatusBar.systemStatusBar()
+    @IBOutlet weak var statusBarItem: NSMenu!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        var userDefaults = NSUserDefaults.standardUserDefaults()
+        let defaultsToRegister = ["DisplayWindowOnStartup": true]
+        userDefaults.registerDefaults(defaultsToRegister)
+        userDefaults.synchronize()
+        
+
+        //Add statusBarIt em
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.title = "Presses"
+        
+        //Add menuItem to menu
+        menuItem.title = "Clicked"
+        menuItem.action = Selector("setWindowVisible:")
+        menuItem.keyEquivalent = ""
+        menu.addItem(menuItem)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func setWindowVisible(sender: AnyObject){
+        self.window!.orderFront(self)
+    }
+    
+    @IBAction func quit(sender: AnyObject?) {
+        self.quit(true)
+    }
 }
 
