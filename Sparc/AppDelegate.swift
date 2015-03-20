db544a8eaa66fc1a5493487ac40509e59ed0e7f7
ArtemTitoulenko <artem.titoulenko@gmail.com>
2015-03-20 17:23:21 -0400

    is able to fetch diffs from Phabricator, prints them out to console!

--- Sparc/AppDelegate.swift
+++ Sparc/AppDelegate.swift
@@ -7,44 +7,178 @@
 //
 
 import Cocoa
+import Foundation
+import Alamofire
+import CryptoSwift
+
+typealias PhabricatorToken = NSString
+
+class Phabricator {
+  var host : NSURL
+  var connected : Bool = false
+  private var username : String?
+  private var certificate : String?
+  private var sessionKey : String?
+  private var connectionID : Int?
+  private var userPHID : String?
+  
+  init(atURL: NSURL) {
+    host = atURL
+  }
+  
+  init(arcRCFilePath: String) {
+    self.host = NSURL(string: "")!
+
+    let jsonData = NSData(contentsOfFile: arcRCFilePath, options: .DataReadingMappedIfSafe, error: nil)
+    var jsonError : NSError?
+    var arc = NSJSONSerialization.JSONObjectWithData(jsonData!, options: .MutableContainers, error: &jsonError) as NSDictionary
+    
+    if let hosts = arc["hosts"]! as? NSDictionary {
+      let key = hosts.allKeys[0] as String
+      let firstHost = hosts.valueForKey(key) as NSDictionary
+      self.host = NSURL(string: key)!
+      
+      if let uname = firstHost["user"] as? String {
+        self.username = uname
+      }
+      
+      if let cert = firstHost["cert"] as? String {
+        self.certificate = cert
+      }
+    } else {
+      NSLog("json error: \(jsonError)")
+    }
+  }
+  
+  // send a request to Conduit. This method json serialzes the Dictionary, and provides a pretty raw callback which will be called
+  // when the request returns
+  private func sendRequest(type: Alamofire.Method, endpoint : String, params : [String:AnyObject],
+    completionHandler: ((NSURLRequest, NSHTTPURLResponse?, AnyObject?, NSError?) -> Void)?) {
+      
+    let requestUrl = host.URLByAppendingPathComponent(endpoint)
+      
+    var localParams = params
+      
+    if self.connected {
+      localParams["__conduit__"] = [
+        "sessionKey": self.sessionKey!,
+        "connectionID": self.connectionID!
+      ]
+    }
+
+    let jsonSerializedConduitParams = NSJSONSerialization.dataWithJSONObject(localParams, options:NSJSONWritingOptions(0), error: nil)
+    let stringEncodedSerializedConduitParams = NSString(data: jsonSerializedConduitParams!, encoding: NSUTF8StringEncoding) as NSString!
+    
+    var parameters : [String: AnyObject] = [
+      "params": stringEncodedSerializedConduitParams,
+      "output": "json",
+      "__conduit__": true
+    ]
+
+    if let handler = completionHandler? {
+      Alamofire.request(.POST, requestUrl, parameters: parameters)
+        .responseJSON(completionHandler: handler)
+    }
+
+  }
+  
+  // Connect to conduit to retrieve a session key, connection ID, and the user PHID
+  func connect(callback: ((NSError!) -> Void)?) {
+    
+    // Create the conduit request to be able to fetch a token
+    let date = NSDate()
+    let token = Int(date.timeIntervalSince1970)
+    var bytes = String(format: "%d%@", token, self.certificate!)
+    let data : NSData = NSData(bytes: &bytes, length: sizeof(bytes.dynamicType))
+    let signature = bytes.sha1() as String!
+    let conduitParameters : [String:AnyObject] = [
+      "client": "Sparc",
+      "clientVersion": 0,
+      "clientDescription": "A menubar app for managing your diffs",
+      "user": username!,
+      "host": String(format: "%@://%@", host.scheme!, host.host!),
+      "authToken": token,
+      "authSignature": signature.lowercaseString
+    ]
+    
+    // Request a connectionID, sessionKey, and the user PHID
+    sendRequest(.POST, endpoint: "conduit.connect", params: conduitParameters) { (_, _, JSON, error) in
+      if error != nil {
+        NSLog("error contacting Phabricator: %@", error!)
+        if let handler = callback {
+          handler(error)
+        }
+        return
+      }
+      
+      // Check if there's an error returned from Phabricator
+      if let error_info = JSON!["error_info"] as? String {
+        NSLog("request error: %@", error_info)
+        return
+      }
+      
+      // The token request was good, save the values
+      if let result = JSON!["result"] as? NSDictionary {
+        self.connectionID = result["connectionID"] as? Int
+        self.sessionKey = result["sessionKey"] as? String
+        self.userPHID = result["userPHID"] as? String
+        
+        self.connected = true
+        if let handler = callback {
+          handler(error)
+        }
+      }
+    }
+  }
+  
+  func getAuthoredDiffs() -> NSError! {
+    if !self.connected {
+      return NSError(domain: "err", code: 2, userInfo: nil)
+    }
+    
+    let queryParams : [String:AnyObject] = [
+      "authors" : NSArray(object: self.userPHID!),
+      "status": "status-open"
+    ]
+  
+    sendRequest(.POST, endpoint: "differential.query", params: queryParams) { (request, _, JSON, error) in
+      if error != nil {
+        NSLog("error contacting Phabricator: %@", error!)
+        return
+      }
+      
+      // Check if there's an error returned from Phabricator
+      if let error_info = JSON!["error_info"] as? String {
+        NSLog("request error: %@", error_info)
+        return
+      }
+      
+      if let result = JSON!["result"] as? NSArray {
+        println("diffs:\n")
+        for diff in result {
+          let ldiff = diff as? NSDictionary
+          let statusName = ldiff?.valueForKey("statusName") as String!
+          let id = ldiff?.valueForKey("id") as String!
+          let title = ldiff?.valueForKey("title") as String!
+          let diffTitle = NSString(format: "%@: %@ - %@", statusName, id, title)
+          println(diffTitle)
+        }
+      }
+    }
+    
+    return nil
+  }
+}
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     var userDefaults = NSUserDefaults.standardUserDefaults()
-    
-    var requestURL : NSURL?
-    
+  
+    var API : Phabricator?
+  
     // Settings window
     @IBOutlet weak var settingsWindow: NSWindow!
-    @IBOutlet var phabricatorUrlField: NSTextField!
-    @IBAction func connectToPhabricator(sender: NSButton) {
-        requestURL = NSURL(string: self.phabricatorUrlField.stringValue)!
-        userDefaults.setValue(self.phabricatorUrlField.stringValue, forKey: "PhabricatorUrl")
-        userDefaults.setValue(self.phabricatorCertificate.stringValue, forKey: "PhabricatorCertificate")
-        userDefaults.synchronize()
-        
-        // TODO -- Create a phabricator connection manager, and just do connection.test(NSURL)?
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(requestURL!) {(data, response, error) in
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
-
-            if let output = (NSString(data: data, encoding: NSUTF8StringEncoding)) {
-                var parseError: NSError?
-                let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parseError)
-                
-                NSLog("%s", parsedObject!.stringValue!)
-            }
-        }
-        
-        task.resume()
-    }
-    
-    @IBOutlet var phabricatorCertificate: NSTextField!
-    @IBAction func fetchCertificatePressed(sender: AnyObject) {
-        let certUrl = requestURL?.URLByAppendingPathComponent("/settings/panel/conduit/")
-        NSWorkspace.sharedWorkspace().openURL(certUrl!)
-    }
     
     // NSMenu
     @IBOutlet weak var statusMenu: NSMenu!
@@ -64,7 +198,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.title = "Sparc";
         statusItem.menu = statusMenu
         
-        requestURL = NSURL(string: userDefaults.stringForKey("PhabricatorUrl")!)!
+        API = Phabricator(arcRCFilePath: "~/.arcrc".stringByExpandingTildeInPath)
+        API!.connect { (error: NSError!) -> Void in
+          self.API?.getAuthoredDiffs()
+          return
+        }
     }
     
     func addMenuItem(title: String, selector: Selector) {
